f87ca7d80f1d62b70d17d1df684ff5bd781f0f7e
Jessica Ohler <jessicaohler@jessicahlersmbp.gateway.pace.com>
2014-07-31 10:59:06 -0400

    string and number functions so far

--- Ohler_Jessica_Assignment4/Ohler_Jessica_Assignment4.js
+++ Ohler_Jessica_Assignment4/Ohler_Jessica_Assignment4.js
@@ -6,7 +6,8 @@
 // Problem-solving functions
 
 // global variables
-var patternNumber = "123-456-7890"
+var patternNumber = "123-456-7890",
+	email = "aaa@bbb.ccc"
 ;
 
 // String Functions
@@ -24,9 +25,47 @@ var patternProblem = function(phoneNumber) {
 	return numberReturn //returns correctly named MAKEUP for Assignment 2
 };
 
+var emailProblem = function(emailInQuestion) {
+	var at = "@",
+		emailReturn;
+	if (emailInQuestion.lastIndexOf(".") > 2) {
+	
+		if (emailInQuestion.indexOf(at) > 0) {
+			emailReturn = true;}
+			
+		}else{
+			emailReturn = false;
+		}
+		return emailReturn;
+};
 
+var urlProblem = function(urlInQuestion) {
+	var urlReturn;
+	if (urlInQuestion.contains("http://" &&! "https://")) { // MAKEUP for Assignment 2: did not use boolean logic operators
+		urlReturn = true;
+	} else {
+		urlReturn = false;
+	}
+	return urlReturn;
+};
+
+var upperCaseFirstLetter = function(string) {
+	var 
+		caseReturn
+	;
+}
+
+var swappingStuff = function (stuff) {
+	var
+		stuffReturn
+	;
+	
+};
 
 // Number Functions
+var roundDecimal = function(number) {
+	return +number.toFixed(2);
+};
 
 
 // Array Functions
@@ -35,3 +74,15 @@ var patternProblem = function(phoneNumber) {
 // main code with my function calls and other outputs
 var chosenNumber = patternProblem("407-401-3456");
 console.log("The string follows a 123-456-7890 pattern like a phone number : " + chosenNumber + ".");
+
+var chosenNumber = patternProblem("4074013456")
+console.log("The string follows a 123-456-7890 pattern like a phone number : " + chosenNumber + ".");
+
+var chosenEmail = emailProblem("j.ohler@fullsail.edu");
+console.log("The string follows an aaa@bbb.ccc pattern just like an email : " + chosenEmail + ".");
+
+var chosenWebsite = urlProblem("fullsail.edu");
+console.log("The string follows the patter of a true URL : " + chosenWebsite + ".");
+
+var numberForRound = roundDecimal(3.26506856);
+console.log("This number is rounded to the second decimal : " + numberForRound + ".");
