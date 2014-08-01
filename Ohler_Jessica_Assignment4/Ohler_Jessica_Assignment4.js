e0142de91ed9ce8f2c0a9a4c382e28735438c9b9
Jessica Ohler <jessicaohler@jessicahlersmbp.gateway.pace.com>
2014-07-31 22:49:46 -0400

    Added more functions

--- Ohler_Jessica_Assignment4/Ohler_Jessica_Assignment4.js
+++ Ohler_Jessica_Assignment4/Ohler_Jessica_Assignment4.js
@@ -68,9 +68,51 @@ var roundDecimal = function(number) {
 	return +number.toFixed(2);
 };
 
+var fuzzyMatch = function() {
+	var array = [5, 10, 50],
+		arrayReturn
+	;
+	if (array[0] < array[1]) {
+		console.log(array[0] + " is less than " + array[1]);
+		if ((array[2] / array[0]) * 100) {
+			console.log(array[0] + " is 50% of " + array[2]);
+	}else{
+		console.log("Did not work.");
+		return arrayReturn;}}
+};
+
+var dates = function() {
+	var anniversary = new Date(2007, 12, 25),
+		today = new Date(2014, 07, 30),
+		oneDay = 1000 * 60 * 60 * 24
+	;
+	yearsTogether = (Math.ceil((today.getTime() - anniversary.getTime()) / oneDay));
+	return yearsTogether
+};
+
+var realNumber = function(stringNumber) {
+	var 
+	change = Math.floor(stringNumber);
+	return change;
+};
 
 // Array Functions
+var smallestValue = function(givenNumber) {
+	var randomArrayOfNumbers = [1, 5, 15, 16, 19, 20, 25]
+	;
+	randomArrayOfNumbers[randomArrayOfNumbers.length] = givenNumber
+	
+	return Math.min.apply( Math, randomArrayOfNumbers)
+};
 
+var sumNumbers = function() {
+	var sillyArray = ["pickles", 7, "14", 2, 3, "Jabba", "montser"],
+	totalVar = 0
+	;
+	Array.prototype.slice[1, 3, 4];
+		sum = sillyArray[1] + sillyArray[3] + sillyArray[4];
+		return sum;
+};
 
 // main code with my function calls and other outputs
 var chosenNumber = patternProblem("407-401-3456");
@@ -88,9 +130,23 @@ console.log("The string follows the patter of a true URL : " + chosenWebsite + "
 var titleCase = upperCaseFirstLetter("my name is jessica");
 console.log(titleCase);
 
-var replace = swappingStuff("Kids Shorten words like text.");
+var replace = swappingStuff("Kids shorten words like text.");
 console.log(replace);
 
 var numberForRound = roundDecimal(3.26506856);
 console.log("This number is rounded to the second decimal : " + numberForRound + ".");
 
+var results = fuzzyMatch();
+console.log(results);
+
+var daysBeenTogether = dates();
+console.log("Jason and I have been together for " +daysBeenTogether + " days.");
+
+var realNumberChanged = realNumber("24");
+console.log("I am now a number : " + realNumberChanged + ".");
+
+var smallestValueIs = smallestValue(42);
+console.log("The smallest number is : " + smallestValueIs + ".");
+
+var sumOfNumbers = sumNumbers();
+console.log("The total sum in my silly array is " + sumOfNumbers + ".");
